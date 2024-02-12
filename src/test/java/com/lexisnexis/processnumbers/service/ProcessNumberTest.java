36b50f7bbbdd570bd887ea7efa99b6e4e1262664
Amol Kshirsagar <amol.kshirsagar@revenue.nsw.gov.au>
2024-02-13 09:13:36 +1100

    refactor

--- src/test/java/com/lexisnexis/processnumbers/service/ProcessNumberTest.java
+++ src/test/java/com/lexisnexis/processnumbers/service/ProcessNumberTest.java
@@ -29,7 +29,7 @@ class ProcessNumberTest {
             "21616177052165224543311447543654741617367042213645643631" +
             "33346575330621635642541636644326535501666004333326756424" +
             "47003252221104064117622317044717471111";
-    List<Integer> decodedNumberList = processNumberService.decodeNumber(String.valueOf(largeIntegerString));
+    List<Integer> decodedNumberList = processNumberService.decodeNumber(largeIntegerString);
     assertTrue(decodedNumberList.size()==140);
     System.out.println("Big Integer Result: " + decodedNumberList);
   }
