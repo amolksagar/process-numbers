ddda091fab2c2fe574767a7c55182a6308350962
Amol Kshirsagar <amol.kshirsagar@revenue.nsw.gov.au>
2024-02-13 09:10:48 +1100

    refactor

--- src/test/java/com/lexisnexis/processnumbers/service/ProcessNumberTest.java
+++ src/test/java/com/lexisnexis/processnumbers/service/ProcessNumberTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 @ExtendWith(SpringExtension.class)
 class ProcessNumberTest {
